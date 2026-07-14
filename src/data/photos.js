8e415aaa19ac92845c904ec8a69544f975be8055
Charlie Macnamara <charlie@charliemacnamara.com>
2026-07-14 19:59:14 +0100

    feat(data): add modular photo registry

---
+++ src/data/photos.js
@@ -0,0 +1,11 @@
+import image1 from '@/images/photos/image-1.jpg'
+import image2 from '@/images/photos/image-2.jpg'
+import image3 from '@/images/photos/image-3.jpg'
+import image4 from '@/images/photos/image-4.jpg'
+
+export const images = [
+  { src: image1 },
+  { src: image2 },
+  { src: image3 },
+  { src: image4 },
+]
